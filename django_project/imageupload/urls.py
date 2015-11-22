bdf1e83bfe3cda7d57d973933966a09e63334feb
Jessica Bell <jecbell@gmail.com>
2015-11-22 23:09:28 +0000

    Temp initial commit

--- django_project/imageupload/urls.py
+++ django_project/imageupload/urls.py
@@ -4,6 +4,7 @@ from imageupload import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login$', views.login, name='login'),
     url(r'^upload$', views.upload, name='upload'),
     url(r'^upload/(?P<ui_id>[0-9]+)$', views.upload_result, name='upload_result'),
 ]
--- django_project/imageupload/views.py
+++ django_project/imageupload/views.py
@@ -6,28 +6,75 @@ from django.http import (
 	Http404,
 )
 from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
 
 from imageupload.models import UploadImage
-from imageupload.forms import UploadImageForm
+from imageupload.forms import (
+	UserForm,
+	UploadImageForm,
+)
 
-# Create your views here.
+
+@login_required
 def index(request):
 	return HttpResponse("Temp index page - will be login.")
+	# if request.method == 'POST':
+	# 	form = UserForm(request.POST, prefix='user')
+	# 	if form.is_valid():
+	# 		user = form.save()
+	# 		user.set_password(user.password)
+	# 		user.save()
+
+	# 		auth_user = authenticate(username=user.username, password=user.password)
+	# 		login(request, user)
+	# 	else:
+	# 		print form.errors
+	# else:
+	# 	if not request.user.is_authenticated():
+	# 		form = UserForm(prefix='user')
+	# 	else:
+	# 		pass
+
+	# return render(request, 'imageupload/index.html', {'form': form})
+
+def login(request):
+	if request.method == 'POST':
+		form = UserForm(request.POST, prefix='user')
+		if form.is_valid():
+			user = form.save()
+			user.set_password(user.password)
+			user.save()
+
+			auth_user = authenticate(username=user.username, password=user.password)
+			login(request, user)
+
+			return HttpResponseRedirect(reverse('index'))
+
+		else:
+			print form.errors
+
+	else:
+		form = UserForm(prefix='user')
+
+	return render(request, 'imageupload/login.html', {'form': form})
+
 
 def upload(request):
 	# Handle file upload
-	    if request.method == 'POST':
-	        form = UploadImageForm(request.POST, request.FILES)
-	        if form.is_valid():
-	            newimage = UploadImage(image = request.FILES['image'])
-	            newimage.save()
+		if request.method == 'POST':
+			form = UploadImageForm(request.POST, request.FILES)
+			if form.is_valid():
+				newimage = UploadImage(image = request.FILES['image'])
+				newimage.save()
 
-	            # Redirect to the upload results after POST
-	            return HttpResponseRedirect(reverse('upload_result', kwargs={'ui_id': newimage.pk}))
-	    else:
-	        form = UploadImageForm() # A empty, unbound form
+				# Redirect to the upload results after POST
+				return HttpResponseRedirect(reverse('upload_result', kwargs={'ui_id': newimage.pk}))
+		else:
+			form = UploadImageForm() # A empty, unbound form
 
-	    return render(request, 'imageupload/upload.html', {'form': form})
+		return render(request, 'imageupload/upload.html', {'form': form})
 
 def upload_result(request, ui_id):
 	try:
